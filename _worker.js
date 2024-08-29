ed9de4695010b049795712e5e9a741a2c3a4f3b9
LXN25565 <lixin25565@gmail.com>
2024-08-29 15:48:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '22a0c87a-6824-4eba-a2f5-892c7a08f0cf';
 
-const พร็อกซีไอพีs = ['2602:294:0:dc:1234:1234:5910:0001'];
+const พร็อกซีไอพีs = ['146.70.175.116'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
