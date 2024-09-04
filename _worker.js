00aa4913c3c6d1c9a8351f40c5cf5d21e1f5bd90
LXN25565 <lixin25565@gmail.com>
2024-09-04 11:46:23 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,9 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '22a0c87a-6824-4eba-a2f5-892c7a08f0cf';
 
-const พร็อกซีไอพีs = ['proxyip.us.fxxk.dedyn.io'];
+const พร็อกซีไอพีs = [
+'proxyip.oracle.fxxk.dedyn.io', 'proxyip.digitalocean.fxxk.dedyn.io', 'proxyip.aliyun.fxxk.dedyn.io'
+];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
